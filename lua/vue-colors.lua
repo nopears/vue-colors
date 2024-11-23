397958c01d4ec8dcdb4249d27e07da45931adda2
nopears <gregorzhilich@gmail.com>
2024-11-23 18:04:13 +0300

    fix

--- lua/vue-colors.lua
+++ lua/vue-colors.lua
@@ -1,25 +1,22 @@
-local function highlight_vue_blocks()
+local M = {}
+
+function M.highlight_vue_blocks()
+  print("Running highlight_vue_blocks")
   vim.cmd([[
     syntax region VueScript start=+<script[^>]*>+ end=+</script>+ contains=ALL
     syntax region VueTemplate start=+<template[^>]*>+ end=+</template>+ contains=ALL
     syntax region VueStyle start=+<style[^>]*>+ end=+</style>+ contains=ALL
 
+    highlight default link VueScript Normal
+    highlight default link VueTemplate Normal
+    highlight default link VueStyle Normal
+
+    " Dark green, dark red, and dark green
     highlight VueScript guibg=#005f5f
     highlight VueTemplate guibg=#5f0000
     highlight VueStyle guibg=#005f00
   ]])
 end
 
-local function setup()
-  local augroup = vim.api.nvim_create_augroup("VueHighlight", { clear = true })
-
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "vue",
-    group = augroup,
-    desc = "Highlight Vue.js code blocks",
-    callback = highlight_vue_blocks,
-  })
-end
-
-return { setup = setup }
+return M
 
