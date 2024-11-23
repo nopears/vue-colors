93052883360bf51bdf7c007b78fa0b89dcc91846
nopears <gregorzhilich@gmail.com>
2024-11-23 17:32:33 +0300

    added au to .lua

--- lua/vue-colors.lua
+++ lua/vue-colors.lua
@@ -2,14 +2,28 @@ local M = {}
 
 function M.highlight_vue_blocks()
 	vim.cmd([[
-    syntax region VueScript start=+<script[^>]*>+ end=+</script>+ contains=ALL
-    syntax region VueTemplate start=+<template[^>]*>+ end=+</template>+ contains=ALL
-    syntax region VueStyle start=+<style[^>]*>+ end=+</style>+ contains=ALL
+    syntax region VueScript start=+<script[^>]*>+ end=+</script>+ containedin=ALL contained
+    syntax region VueTemplate start=+<template[^>]*>+ end=+</template>+ containedin=ALL contained
+    syntax region VueStyle start=+<style[^>]*>+ end=+</style>+ containedin=ALL contained
+
+    highlight default link VueScript Normal
+    highlight default link VueTemplate Normal
+    highlight default link VueStyle Normal
 
     highlight VueScript guibg=#FFDDDD
     highlight VueTemplate guibg=#DDFFDD
     highlight VueStyle guibg=#DDDDFF
   ]])
 end
+
+-- Autocommand setup directly in Lua
+vim.api.nvim_create_augroup("VueHighlight", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "vue",
+	callback = function()
+		M.highlight_vue_blocks()
+	end,
+	group = "VueHighlight",
+})
 
 return M
