31309457a523861b91ef66ccef1e0163250e8288
nopears <gregorzhilich@gmail.com>
2024-11-23 17:43:27 +0300

    ?

--- lua/vue-colors.lua
+++ lua/vue-colors.lua
@@ -2,17 +2,13 @@ local M = {}
 
 function M.highlight_vue_blocks()
 	vim.cmd([[
-    syntax region VueScript start=+<script[^>]*>+ end=+</script>+ containedin=ALL contained
-    syntax region VueTemplate start=+<template[^>]*>+ end=+</template>+ containedin=ALL contained
-    syntax region VueStyle start=+<style[^>]*>+ end=+</style>+ containedin=ALL contained
+    syntax region VueScript start=+<script[^>]*>+ end=+</script>+ contains=ALL
+    syntax region VueTemplate start=+<template[^>]*>+ end=+</template>+ contains=ALL
+    syntax region VueStyle start=+<style[^>]*>+ end=+</style>+ contains=ALL
 
-    highlight default link VueScript Normal
-    highlight default link VueTemplate Normal
-    highlight default link VueStyle Normal
-
-    highlight VueScript guibg=#005f5f
-    highlight VueTemplate guibg=#5f0000
-    highlight VueStyle guibg=#005f00
+    highlight VueScript guibg=#FFDDDD
+    highlight VueTemplate guibg=#DDFFDD
+    highlight VueStyle guibg=#DDDDFF
   ]])
 end
 
